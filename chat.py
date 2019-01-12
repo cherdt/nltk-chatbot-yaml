eb59195e5e89ff65b72b3d1173c3e8ab155c199f
Chris Herdt <cherdt@gmail.com>
2019-01-12 12:35:21 -0500

    Add feature to get bot source from a URL

--- chat.py
+++ chat.py
@@ -1,23 +1,39 @@
 import argparse
 import nltk.chat
 from nltk.chat.util import Chat, reflections
+import requests
 import yaml
 
+def get_chatbot_source_from_file(filepath):
+    try:
+        with open(filepath) as f:
+            chatbot_src = f.read()
+    except:
+        print("ERROR: unable to open " + filepath)
+        exit(1)
+    return chatbot_src
+
+def get_chatbot_source_from_url(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        print("ERROR: unable to open " + url)
+        exit(1)
+    return r.text
 
 def main():
 
     # create arg parser
     parser = argparse.ArgumentParser(description='Talk with a chatbot.')
-    parser.add_argument('-f', '--file', dest="file", help='YAML file with chatbot source')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-f', '--file', dest='file', help='YAML file with chatbot source')
+    group.add_argument('-u', '--url', dest='url', help='URL of a chatbot source file')
+
     args = parser.parse_args()
 
-    # open the source file
-    try:
-        with open(args.file) as f:
-            chatbot_src = f.read()
-    except:
-        print("ERROR: unable to open " + args.file)
-        exit(1)
+    if args.file:
+        chatbot_src = get_chatbot_source_from_file(args.file)
+    if args.url:
+        chatbot_src = get_chatbot_source_from_url(args.url)
 
     # load the source file
     try:
